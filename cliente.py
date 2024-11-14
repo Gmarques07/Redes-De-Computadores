053ca409a03d284edfdb7955a564190a9717f007
Gmarques07 <amsgabriel@hotmail.com>
2024-11-13 21:56:41 -0300

    Chat – Versão 2

--- cliente.py
+++ cliente.py
@@ -13,10 +13,14 @@ def escutar_servidor(s):
             print("Servidor desconectado.")
             break
 
+
 def enviar_para_servidor(s):
     while True:
         try:
             mensagem = input("Digite sua mensagem para o servidor: ")
+            if mensagem.lower() == 'sair':
+                s.send('Cliente desconectado'.encode())
+                break
             s.send(mensagem.encode())
         except BrokenPipeError:
             print("Conexão com o servidor foi perdida.")
@@ -26,14 +30,16 @@ def enviar_para_servidor(s):
             s.close()
             break
 
+
 s = socket(AF_INET, SOCK_STREAM)
 s.connect(('127.0.0.1', 8000))
 
 
-nome_cliente = input("Digite seu nome: ")
+nome_cliente = input("Identifique-se: ")
 s.send(nome_cliente.encode())
 
 print(f'Conectado ao servidor na porta 8000 como {nome_cliente}')
+
 
 Thread(target=escutar_servidor, args=(s,)).start()
 Thread(target=enviar_para_servidor, args=(s,)).start()
--- servidor.py
+++ servidor.py
@@ -1,28 +1,41 @@
 from socket import socket, AF_INET, SOCK_STREAM
 from threading import Thread
 
+clientes_conectados = []
+
+
+def enviar_mensagem_para_cliente(mensagem, cliente_socket):
+    try:
+        cliente_socket.send(mensagem.encode())
+    except BrokenPipeError:
+        print(f"Erro ao enviar mensagem para o cliente.")
+
+
 def conexao_cliente(cliente_socket, endereco_cliente):
-    cliente_socket.send("Identifique-se".encode())
+    # Solicita o nome do cliente
+    cliente_socket.send("Identifique-se: ".encode())
     nome_cliente = cliente_socket.recv(1500).decode()
     print(f'Conexão estabelecida com o cliente {nome_cliente} ({endereco_cliente})')
-    
-    while True:
-        try:
+
+    clientes_conectados.append(cliente_socket)
+
+    try:
+        while True:
             mensagem = cliente_socket.recv(1500)
             if not mensagem:
                 print(f'Cliente {nome_cliente} desconectado.')
                 break
-            
+
             print(f'Mensagem recebida de {nome_cliente} ({endereco_cliente}): {mensagem.decode()}')
 
-            resposta = input(f"Respota para o cliente {nome_cliente} ({endereco_cliente}): ")
+            resposta = input(f"Digite sua resposta para o cliente {nome_cliente} ({endereco_cliente}): ")
             cliente_socket.send(resposta.encode())
-        
-        except ConnectionResetError:
-            print(f"Cliente {nome_cliente} desconectado.")
-            break
-    cliente_socket.close()
+    except ConnectionResetError:
+        print(f"Cliente {nome_cliente} desconectado abruptamente.")
+    finally:
 
+        clientes_conectados.remove(cliente_socket)
+        cliente_socket.close()
 
 server_socket = socket(AF_INET, SOCK_STREAM)
 server_socket.bind(('127.0.0.1', 8000))
@@ -30,6 +43,10 @@ server_socket.listen()
 print('Servidor está pronto e aguardando conexões na porta 8000')
 
 
-while True:
-    cliente_socket, endereco_cliente = server_socket.accept()
-    Thread(target=conexao_cliente, args=(cliente_socket, endereco_cliente)).start()
+def gerenciar_conexoes():
+    while True:
+        cliente_socket, endereco_cliente = server_socket.accept()
+        print(f"Cliente {endereco_cliente} conectado.")
+        Thread(target=conexao_cliente, args=(cliente_socket, endereco_cliente)).start()
+
+Thread(target=gerenciar_conexoes).start()
